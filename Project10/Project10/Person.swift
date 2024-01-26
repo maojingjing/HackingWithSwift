f674b34e8282c10d686540a0a92b812f1ff355ba
jjm <mjj0131@gmail.com>
2024-01-25 21:47:59 -0800

    project 10 image picker

---
+++ Project10/Project10/Person.swift
@@ -0,0 +1,20 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by Jingjing Mao on 1/25/24.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+    
+
+}
